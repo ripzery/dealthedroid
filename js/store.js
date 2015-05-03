e0eb1c25d8ace90f1ab422d71c9abf32d420b59a
phuchit <ripzery@gmail.com>
2015-05-03 13:27:14 +0700

    update store and cart system

--- js/store.js
+++ js/store.js
@@ -5,8 +5,8 @@ var app = angular.module('store', ['ngRoute','ngCart']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-        .when('/main', {
-            templateUrl: 'main.html',
+        .when('/store', {
+            templateUrl: 'store.html',
             controller: 'storeController'
         })
         .when('/cart', {
@@ -19,6 +19,23 @@ app.controller('storeController',function($scope,$location,$http){
     $scope.isActive = function (route) {
         return route === $location.path();
     };
+
+    $scope.mobiles = [];
+    $scope.loadMobiles = function() {
+        return $scope.mobiles.length ? null : $http.get('../database/inventory.php').success(function(data) {
+            $scope.mobiles = data;
+        });
+    };
+
+    $scope.$on('$viewContentLoaded', function() {
+        $scope.loadMobiles();
+    });
+    //
+    //$http.post('../database/inventory.php')
+    //    .done(function (result) {
+    //        $scope.mobiles = result;
+    //        $scope.$apply();
+    //    });
 });
 
 app.controller('cartController', function ($scope, $location) {
