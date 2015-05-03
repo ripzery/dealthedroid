e86a0631869c975c24c0b3cf60eea61099875f8b
phuchit <ripzery@gmail.com>
2015-05-03 10:31:56 +0700

    update store page

--- js/store.js
+++ js/store.js
@@ -1,13 +1,17 @@
 /**
  * Created by visit on 5/3/15 AD.
  */
-var app = angular.module('store', ['ngRoute', 'angularUtils.directives.dirPagination','ngCart']);
+var app = angular.module('store', ['ngRoute','ngCart']);
 
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/main', {
             templateUrl: 'main.html',
             controller: 'storeController'
+        })
+        .when('/cart', {
+            templateUrl: 'cart.html',
+            controller: 'cartController'
         });
 });
 
@@ -15,4 +19,10 @@ app.controller('storeController',function($scope,$location,$http){
     $scope.isActive = function (route) {
         return route === $location.path();
     };
+});
+
+app.controller('cartController', function ($scope, $location) {
+    $scope.isActive = function (route) {
+        return route === $location.path();
+    }
 });
