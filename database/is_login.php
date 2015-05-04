845d31cba69794f447104980c6b20ccb8a0f3f24
phuchit <ripzery@gmail.com>
2015-05-04 08:50:34 +0700

    fix minor bug add login user

--- database/is_login.php
+++ database/is_login.php
@@ -6,6 +6,7 @@ session_start();
 if(isset($_SESSION['logged_in'])){
     if($_SESSION['uid'] == 1){ // admin
         echo "admin";
+        return;
     }else{ // user
        $user = R::load('users', $_SESSION['uid']);
         echo $user->username;
